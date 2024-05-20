cffac65dda383c491fbf9cfa5d6c33240aa7f180
Полина Артамонова <poliiiiinka71@gmail.com>
2024-05-20 16:47:35 +0300

    Добавьте файлы проекта.

---
+++ SF_HW03_ModuleTwelve/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SF_HW03_ModuleTwelve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User()
+            {
+                Name = "Владислав",
+                Login = "mr.vlad",
+                Subscription = false
+            };
+
+            User user2 = new User()
+            {
+                Name = "Кирилл",
+                Login = "abramov.k",
+                Subscription = true
+            };
+
+            // добавляем пользователей в список
+            var Users = new List<User>() { user1, user2 };
+
+            foreach (var user in Users)
+            {
+                Console.WriteLine($"Здравствуйте, {user.Name}!");
+                if (user.Subscription != true)
+                    ShowAds();
+                Console.WriteLine();
+            }
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ SF_HW03_ModuleTwelve/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF_HW03_ModuleTwelve
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public bool Subscription { get; set; }
+    }
+}
